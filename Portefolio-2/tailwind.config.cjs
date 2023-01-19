bcf7c8d48a078d1995b5074d384fc8becbb8d23a
snorre kolnes røgenes <snorre.kolnes@hotmail.com>
2023-01-19 18:07:26 +0100

    HTML and css 1

--- Portefolio-2/tailwind.config.cjs
+++ Portefolio-2/tailwind.config.cjs
@@ -10,10 +10,22 @@ module.exports = {
       },
       colors: {
         "FRAM-LIGHT-BROWN": "#CB8677",
-        "FRAM-PINK": "#DO9E93",
+        "FRAM-PINK": "#D09E93",
         "FRAM-DARK-BROWN": "#271A14",
         "FRAM-WHITE": "#E9E5E4",
         "FRAM-TEAL": "#7BA69A",
+      }, screens: {
+
+        'lMobile': '425px',
+
+        'tablet': '740px',
+        // => @media (min-width: 640px) { ... }
+                                          
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
       }
     },
   },
